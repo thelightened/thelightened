81ccadb739c79675f25bbcc31173300d5760c482
Edward14 <smallsodagreen@gmail.com>
2016-11-02 23:41:38 +0800

    -a

--- thelightened(django)/mysite/mysite/urls.py
+++ thelightened(django)/mysite/mysite/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from mysite.views import here
+from views import login,indexhomepage
 from cafe.views import index, test
 
 urlpatterns = patterns('',
    
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^here/',here),
     url(r'^index/$',index),
-    url(r'^test/$',test)
+    url(r'^test/$',test),
+    url(r'^login/$',login),
+    url(r'^indexhomepage/$',indexhomepage),	
 )
